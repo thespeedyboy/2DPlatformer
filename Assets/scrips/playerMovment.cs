46135f035a654b2138190b22b395b3362c45a4c4
Travis m <murdocktravis03@gmail.com>
2025-02-07 09:17:43 -0800

    player movment and tiles

--- Assets/scrips/playerMovment.cs
+++ Assets/scrips/playerMovment.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class playerMovment : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Horizontal Movment")]
+    public float Speed;
+    private float horizontalInput;
+    [Header("Jumping")]
+    public float jumpForce;
+    private Rigidbody2D rb;
+    private bool isOnGround = true;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();  
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        //Horizontal Movement
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * Speed * Time.deltaTime * horizontalInput);
+        //jumping
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            isOnGround = false;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
 }
